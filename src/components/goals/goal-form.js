57e7fe275362115e91419ef614a6e07dfed44a31
Ilya1510 <ilya151098@yandex.ru>
2020-12-01 21:15:37 +0300

    add/delete goal

---
+++ src/components/goals/goal-form.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react';
+import {Button, FormGroup, Grid, TextField, Typography} from "@material-ui/core";
+import {connect} from "react-redux";
+import {addGoal} from "../../actions";
+import {withRouter} from "react-router";
+
+class GoalForm extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: '',
+            description: ''
+        }
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if (this.props.addedGoal === this.state.title + this.state.description) {
+            this.props.history.push('/goals');
+        }
+    }
+
+    handleChange = (e) => {
+        const {value, id} = e.target
+        this.setState({
+            [id]: value
+        })
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        this.props.addGoal(this.state);
+    }
+
+    render() {
+        return (
+            <div>
+                <Grid container alignItems="center">
+                    <FormGroup>
+                        <TextField
+                            margin="dense"
+                            id="title"
+                            label="title"
+                            type="text"
+                            name="title"
+                            multiline={true}
+                            value={this.state.title}
+                            onChange={this.handleChange}
+                            InputLabelProps={{
+                                shrink: true,
+                            }}
+                            variant="outlined"
+                        />
+                        <TextField
+                            margin="dense"
+                            id="description"
+                            label="description"
+                            type="text"
+                            name="description"
+                            multiline={true}
+                            value={this.state.description}
+                            onChange={this.handleChange}
+                            InputLabelProps={{
+                                shrink: true,
+                            }}
+                            variant="outlined"
+                        />
+                        <Button color="primary" onClick={this.handleSubmit}>submit</Button>
+                    </FormGroup>
+                </Grid>
+            </div>
+        )
+    }
+}
+
+const mapStateToProps = ({goal: {addedGoal}}) => {
+    return {addedGoal}
+}
+
+export default connect(mapStateToProps, {addGoal})(withRouter(GoalForm));
